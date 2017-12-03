c4947c334402f1fba9f751d3060a5a65d0cee75a
sinouye <sean.inouye@wsu.edu>
2017-12-03 09:47:15 -0800

    Updated TAMyInfo page

--- static/scripts/Application.js
+++
@@ -1,8 +0,0 @@
-function showDiv(elem){
-	if(elem.value == 1)
-	{document.getElementById('courseTime').style.display = "block";}
-	else if(elem.value == 2)
-	{
-		document.getElementById('courseTime').style.display = "none";
-	}
-}
---
+++ static/scripts/TAMyInfo.js
@@ -0,0 +1,6 @@
+function myFunction() {
+	$(document).ready(function(){
+		$('.select').material_select();
+	});
+}
+
